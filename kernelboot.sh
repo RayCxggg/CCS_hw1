dbcb51e025c0be9edfb2ef43c7bdb5571bf7c5c0
RayCxggg <chexj18@163.com>
2022-11-28 07:33:40 +0000

    add kernel boot script

---
+++ kernelboot.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+qemu-system-x86_64 \
+    -m 64M \
+    -nographic \
+    -kernel ./bzImage \
+    -initrd  ./rootfs.img \
+    -append "root=/dev/ram rw console=ttyS0 oops=panic panic=1 kaslr" \
+    -smp cores=2,threads=1 \
+    -cpu kvm64

4bc07e59b4b1bf82373e5d468a6c19178557089a
RayCxggg <chexj18@163.com>
2022-12-21 02:40:20 +0000

    add new sample

--- virtualization/boot.sh
+++ virtualization/boot.sh
@@ -1,8 +1,8 @@
 qemu-system-x86_64 \
     -m 64M \
     -nographic \
-    -kernel ./bzImage \
-    -initrd  ./rootfs.img \
-    -append "root=/dev/ram rw console=ttyS0 oops=panic panic=1 kaslr" \
+    -kernel bzImage \
+    -initrd  rootfs.img \
+    -append "root=/dev/null rw console=ttyS0 oops=panic panic=1 kaslr" \
     -smp cores=2,threads=1 \
     -cpu kvm64
